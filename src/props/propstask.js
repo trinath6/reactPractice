01edd7894717e45673b149c186c1b4a99a380880
trinath6 <trinathreddy6650@gmail.com>
2024-05-08 11:53:55 +0530

    using props displayed information in flex

---
+++ src/props/propstask.js
@@ -0,0 +1,20 @@
+import "./props.css"
+
+let Card=(props)=>{
+    let {category,image,price,title}=props
+    return(
+        <div class="full">
+         <div className="card" style={{ width: 400 }}>
+           <img className="card-img-top" style={{height:200,width:200}} src={image} alt="Card image" />
+           <div className="card-body">
+              <h4 className="card-title">{category}</h4>
+              <p className="card-text">{title}</p>
+              <p className="card-text">{price}</p>
+              <a href="#" className="btn btn-primary">See Profile</a>
+            </div>
+        </div>
+        </div>
+    )
+}
+
+export default Card
