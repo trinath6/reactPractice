e89c4284d6d561c66a53c2b88921031978f872b6
trinath6 <trinathreddy6650@gmail.com>
2024-05-17 10:36:31 +0530

    fix

---
+++ src/state/state.js
@@ -0,0 +1,18 @@
+import { Component } from 'react';
+import axios from 'axios';
+
+class Hello extends Component {
+    state={
+        count:1
+    }
+    render() {
+        return (
+            <>
+             {/* <button onClick={}>subscribe</button> */}
+            </>
+          
+        )
+    }
+}
+
+export default Hello;
